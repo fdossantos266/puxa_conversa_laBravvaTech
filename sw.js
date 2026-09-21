4eae641ec6ee6c39f0f0c391b44d9bdfabeac1de
fdossantos266 <f.dossantosalves97351@gmail.com>
2026-09-21 16:01:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Nome do cache. Mude o "v1" sempre que alterar o site,
 // senão os usuários continuam vendo a versão antiga guardada.
-const CACHE = 'puxa-conversa-v12';
+const CACHE = 'puxa-conversa-v13';
 
 // Arquivos guardados na instalação
 const ARQUIVOS = [
